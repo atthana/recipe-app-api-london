3d36e6d19474413b24da1c0b4d57d5ffdcdb3d88
atthana-p <atthana.p@codium.co>
2022-03-06 21:43:42 +0700

    EP32-33: Add tests for listing users in Django admin คือ เราจะ update Django admin เพื่อให้สามารถ custom user model ได้ (เริ่มจากเขียนเทสเหมือนเดิม) แล้วก้อมา custom ที่  admin.py เพื่อให้ที่เขียนเทสไว้ผ่าน

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.Client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@engineer.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@engineer.com',
+            password='password123',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')  # url แบบนี้ก้อมาจากใน doc นั่นแหละ หรือเราจะ show_urls ดูก้อได้นะ
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)  # เช็คว่า response มันมี name กับ email อยุ่ด้วยรึป่าว
+        self.assertContains(res, self.user.email)
