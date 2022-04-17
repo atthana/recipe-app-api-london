0752eb893e183d50956e58603eff99f549c94807
atthana-p <atthana.p@codium.co>
2022-04-18 02:10:14 +0700

    EP58: Add create tags feature (อันนี้แค่เพิ่มให้ POST ให้ได้นะ จากนั้นแรกที่ api  นี้ GET ได้อย่างเดียว)

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,9 +6,11 @@ from core.models import Tag
 from .serializers import TagSerializer
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
-    # authentication_classes = (TokenAuthentication,)  # อันนี้เรา comment ไปก็ได้นะ ถ้าเราไม่อยากใช้ ModHeader ในการจัดการกับ token
+    authentication_classes = (TokenAuthentication,)  # อันนี้เรา comment ไปก็ได้นะ ถ้าเราไม่อยากใช้ ModHeader ในการจัดการกับ token
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
@@ -16,3 +18,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):  # อันนี้ไม่เคยใช้เลย ต่างจาก create() ยังไงหว่า ที่เห็นชัดๆเลยคือ อันนี้มันรับเป็น serializer เข้ามา แทนที่จะเป็น request นะ
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
