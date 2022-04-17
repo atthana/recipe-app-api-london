c4ba9a101f15d036607464b47c05fac45cc97132
atthana-p <atthana.p@codium.co>
2022-04-17 17:54:29 +0700

    EP54-56: Add tag model (สร้างแอพใหม่ชื่อ recipe, register admin, สร้าง Tag model, make migrations, สร้างเทสของ test_tags_api ขึ้นมานะ แม้ว่ายังพังอยู่

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
@@ -23,6 +24,7 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
@@ -33,3 +35,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
